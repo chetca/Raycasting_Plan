77a4b6a57ceeff9f1cd1c02593db6786352e4955
Alexander Shornikov <chetca@yandex.ru>
2017-02-23 00:12:43 +0800

    test branch

--- raycastingpainter.cpp
+++ raycastingpainter.cpp
@@ -9,6 +9,11 @@ void RaycastingPainter::paint(QWidget *widget, QPointF position, QPointF directi
 {
     //описать алгоритм как бросать луч
     //по возможности передать усилия на другие методы
+
+    //Итак, можно сделать буфер. Это даст плавность кадров, но рендеринг будет медленнее
+    if (buffer.size() != size())
+        buffer = QImage(size(), QImage::Format_ARGB32);
+    castRays(position, direction, fow, width);
 }
 
 Scene *RaycastingPainter::scene() const
@@ -19,4 +24,50 @@ Scene *RaycastingPainter::scene() const
 void RaycastingPainter::setScene(Scene *scene)
 {
     m_scene = scene;
+}
+
+void RaycastingPainter::castRays(QPointF position, QPointF direction, double fov, double width)
+{
+    //Метод описывающий бросание лучей
+    double angleBetweenRays = ((double)direction*(180/Math.PI) / width)*Math.PI /180; //делим угол обзора на маленьки кусочки - углы куда будут бросаться лучи
+    double dist;   //переменная расстояния до препятствия
+    double angle = fov/2 + (double)position; //(pos+dir+plane)
+    if(angle < 0) //Если получившийся угол отрицательный
+        angle += 360 * Math.PI /180;//поворачиваем его на 2*Pi по часовой
+    if(angle > 0) //Если получившийся угол положительный
+        angle -= 360 * Math.PI /180; //поворачиваем его на 2*Pi против часовой
+
+      for (int i = 0; i < width;i++){
+        //Бросаем по одному лучу на каждый кусочек экрана и переходим на следующий кусочек
+    }
+}
+
+double RaycastingPainter::getFov() const
+{
+    return fov;
+}
+
+void RaycastingPainter::setFov(double value)
+{
+    fov = value;
+}
+
+double RaycastingPainter::getWidth() const
+{
+    return width;
+}
+
+void RaycastingPainter::setWidth(double value)
+{
+    width = value;
+}
+
+QImage RaycastingPainter::getBuffer() const
+{
+    return buffer;
+}
+
+void RaycastingPainter::setBuffer(const QImage &value)
+{
+    buffer = value;
 }
--- raycastingpainter.h
+++ raycastingpainter.h
@@ -15,9 +15,22 @@ public:
 
     Scene *scene() const;
     void setScene(Scene *scene);
+    void castRays(QPointF position, QPointF direction, double fov, double width);
+
+    double getFov() const;
+    void setFov(double value);
+
+    double getWidth() const;
+    void setWidth(double value);
+
+    QImage getBuffer() const;
+    void setBuffer(const QImage &value);
 
 private:
     Scene *m_scene = 0;
+    QImage buffer;
+    double fov; //Угол обзора
+    double width;
 };
 
 #endif // RAYCASTINGPAINTER_H
